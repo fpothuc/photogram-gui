66cd4660692616582ad8b93206134e0c4a892a1b
fpothuc <164956630+fpothuc@users.noreply.github.com>
2024-05-07 18:28:44 +0000

    adding individual user pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get("/", {:controller => "users", :action => "index"})
 
   get("/users", {:controller => "users", :action => "index"})
+  get("/users/", {:controller => "users", :action => "index"})
 
   get("/photos", {:controller => "photos", :action => "index"})
 end
